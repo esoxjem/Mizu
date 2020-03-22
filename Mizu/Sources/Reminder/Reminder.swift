cbb0743f0847f2f3e665e458d71178d7721629e4
Arun Sasidharan <arunsasidharan121@gmail.com>
2020-03-22 19:30:23 +0100

    fix sound pref

--- Mizu/Sources/Reminder/Reminder.swift
+++ Mizu/Sources/Reminder/Reminder.swift
@@ -3,6 +3,7 @@ import Foundation
 class Reminder {
     
     private var timer: Timer?
+    private let preferences = Preferences()
     
     func startTimer() {
         let intervalInSecs = Interval().seconds()
@@ -15,11 +16,14 @@ class Reminder {
     @objc private func showNotification() {
         let notification = NSUserNotification()
         notification.title = "Time to drink water"
-        notification.informativeText = "It's been \(Interval().string()) since your last cup."
-        notification.soundName = NSUserNotificationDefaultSoundName
+        notification.informativeText = "It's been \(Interval().string()) since your last glass."
         notification.hasActionButton = true
         notification.otherButtonTitle = "Dismiss"
         notification.actionButtonTitle = "OK"
+        
+        if preferences.isSoundEnabled() {
+            notification.soundName = NSUserNotificationDefaultSoundName
+        }
         
         let notificationCenter = NSUserNotificationCenter.default
         notificationCenter.deliver(notification)
